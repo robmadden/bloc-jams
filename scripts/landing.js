ba1cabbfe605d24d6bf4ff8033632ed920f67fff
Dan <Agarman1@users.noreply.github.com>
2016-08-18 20:49:42 -0500

    Assigment DOM Events: Completed

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,17 +1,14 @@
 var pointsArray = document.getElementsByClassName('point');
 
+var revealPoint = function(point) {
+    point.style.opacity = 1;
+    point.style.transform = "scaleX(1) translateY(0)";
+    point.style.msTransform = "scaleX(1) translateY(0)";
+    point.style.WebkitTransform = "scaleX(1) translateY(0)";
+}
+
 var animatePoints = function(points) {
-
-var revealPoint = function(index) {
-    points[index].style.opacity = 1;
-    points[index].style.transform = "scaleX(1) translateY(0)";
-    points[index].style.msTransform = "scaleX(1) translateY(0)";
-    points[index].style.WebkitTransform = "scaleX(1) translateY(0)";
-  }
-
-  for (var i = 0; i < points.length; i++) {
-      revealPoint(i);
-  }
+    forEach(points, revealPoint)
 
 };
 
@@ -19,6 +16,7 @@ window.onload = function() {
     if (window.innerHeight > 950) {
         animatePoints(pointsArray);
     }
+
     var sellingPoints = document.getElementsByClassName('selling-points')[0];
     var scrollDistance = sellingPoints.getBoundingClientRect().top - window.innerHeight + 200;
 
