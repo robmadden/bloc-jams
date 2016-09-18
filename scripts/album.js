a7cb569e884d3a798d4cbb085bfc7fb5e4ac2191
Dan <Agarman1@users.noreply.github.com>
2016-09-18 15:02:03 -0500

    Checkpoint 20 jQuery Buzz Library: completed

--- scripts/album.js
+++ scripts/album.js
@@ -1,7 +1,21 @@
 var setSong = function(songNumber) {
+    if (currentSoundFile) {
+        currentSoundFile.stop();
+    }
     currentlyPlayingSongNumber = parseInt(songNumber);
     currentSongFromAlbum = currentAlbum.songs[songNumber - 1];
+    currentSoundFile = new buzz.sound(currentSongFromAlbum.audioUrl, {
+    formats: [ 'mp3' ],
+    preload: true
+});
 
+    setVolume(currentVolume);
+};
+
+var setVolume = function(volume) {
+    if (currentSoundFile) {
+        currentSoundFile.setVolume(volume);
+    }
 };
 
 var getSongNumberCell = function(number) {
@@ -30,12 +44,18 @@ var createSongRow = function(songNumber, songName, songLength) {
       	if (currentlyPlayingSongNumber !== songNumber) {
     	    	$(this).html(pauseButtonTemplate);
     		    setSong(songNumber);
+            currentSoundFile.play();
             updatePlayerBarSong();
     	  } else if (currentlyPlayingSongNumber === songNumber) {
-    		    $(this).html(playButtonTemplate);
-            $('.main-controls .play-pause').html(playerBarPlayButton);
-            setSong(null);
-    	  }
+            if (currentSoundFile.isPaused()) {
+                $(this).html(pauseButtonTemplate);
+                $('.main-controls .play-pause').html(playerBarPauseButton);
+                currentSoundFile.play();
+            } else {
+                $(this).html(playButtonTemplate);
+                $('.main-controls .play-pause').html(playerBarPlayButton);
+                currentSoundFile.pause();
+            }
     };
 
     var onHover = function(event) {
@@ -103,6 +123,7 @@ var nextSong = function() {
     }
 
     setSong(currentSongIndex + 1);
+    currentSoundFile.play();
 
     $('.currently-playing .song-name').text(currentSongFromAlbum.title);
     $('.currently-playing .artist-name').text(currentAlbum.artist);
@@ -132,6 +153,7 @@ var previousSong = function() {
     }
 
     setSong(currentSongIndex + 1);
+    currentSoundFile.play();
 
     $('.currently-playing .song-name').text(currentSongFromAlbum.title);
     $('.currently-playing .artist-name').text(currentAlbum.artist);
@@ -180,6 +202,8 @@ var playerBarPauseButton = '<span class="ion-pause"></span>';
 var currentAlbum = null;
 var currentlyPlayingSongNumber = null;
 var currentSongFromAlbum = null;
+var currentSoundFile = null;
+var currentVolume = 80;
 
 var $previousButton = $('.main-controls .previous');
 var $nextButton = $('.main-controls .next');
